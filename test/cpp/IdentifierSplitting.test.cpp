e86c28b0913be6221c587c34bbe5d068537f9c0b
Aaron Ballman <aballman@grammatech.com>
2020-02-25 10:24:02 -0500

    Add an initial implementation for identifier splitting.

---
+++ test/cpp/IdentifierSplitting.test.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "IdentifierSplitting.hpp"
+
+TEST(IdentifierSplitting, split) {
+  IdentifierSplitter Splitter;
+
+  EXPECT_THAT(Splitter.split("foo_bar"), testing::ElementsAre("foo", "bar"));
+  EXPECT_THAT(Splitter.split("fooBar"), testing::ElementsAre("foo", "Bar"));
+  EXPECT_THAT(Splitter.split("Foobar"), testing::ElementsAre("Foobar"));
+  EXPECT_THAT(Splitter.split("fooBarBaz"),
+              testing::ElementsAre("foo", "Bar", "Baz"));
+  EXPECT_THAT(Splitter.split("foo_barBaz"),
+              testing::ElementsAre("foo", "bar", "Baz"));
+
+  EXPECT_THAT(Splitter.split("__foobar"), testing::ElementsAre("foobar"));
+  EXPECT_THAT(Splitter.split("foobar__"), testing::ElementsAre("foobar"));
+  EXPECT_THAT(Splitter.split("foo__bar"), testing::ElementsAre("foo", "bar"));
+
+  EXPECT_THAT(Splitter.split("FOOBAR"), testing::ElementsAre("FOOBAR"));
+  EXPECT_THAT(Splitter.split("fooBAR"), testing::ElementsAre("foo", "BAR"));
+  EXPECT_THAT(Splitter.split("FOObar"), testing::ElementsAre("FOObar"));
+}
+
