a3fcf7f6a53239fd2c193fb6ad86e01cb44a2ede
Aaron Ballman <aballman@grammatech.com>
2020-04-15 14:11:46 -0400

    Now, with passing tests.
    IP:STAMP

--- test/cpp/IdentifierSplitting.test.cpp
+++ test/cpp/IdentifierSplitting.test.cpp
@@ -7,19 +7,31 @@ using namespace swapped_arg;
 TEST(IdentifierSplitting, split) {
   IdentifierSplitter Splitter;
 
-  EXPECT_THAT(Splitter.split("foo_bar"), testing::ElementsAre("foo", "bar"));
-  EXPECT_THAT(Splitter.split("fooBar"), testing::ElementsAre("foo", "Bar"));
-  EXPECT_THAT(Splitter.split("Foobar"), testing::ElementsAre("Foobar"));
+  EXPECT_THAT(Splitter.split("foo_bar"),
+              testing::UnorderedElementsAre("foo", "bar"));
+  EXPECT_THAT(Splitter.split("fooBar"),
+              testing::UnorderedElementsAre("foo", "Bar"));
+  EXPECT_THAT(Splitter.split("Foobar"),
+              testing::UnorderedElementsAre("Foobar"));
   EXPECT_THAT(Splitter.split("fooBarBaz"),
-              testing::ElementsAre("foo", "Bar", "Baz"));
+              testing::UnorderedElementsAre("foo", "Bar", "Baz"));
   EXPECT_THAT(Splitter.split("foo_barBaz"),
-              testing::ElementsAre("foo", "bar", "Baz"));
+              testing::UnorderedElementsAre("foo", "bar", "Baz"));
 
-  EXPECT_THAT(Splitter.split("__foobar"), testing::ElementsAre("foobar"));
-  EXPECT_THAT(Splitter.split("foobar__"), testing::ElementsAre("foobar"));
-  EXPECT_THAT(Splitter.split("foo__bar"), testing::ElementsAre("foo", "bar"));
+  EXPECT_THAT(Splitter.split("__foobar"),
+              testing::UnorderedElementsAre("foobar"));
+  EXPECT_THAT(Splitter.split("foobar__"),
+              testing::UnorderedElementsAre("foobar"));
+  EXPECT_THAT(Splitter.split("foo__bar"),
+              testing::UnorderedElementsAre("foo", "bar"));
 
-  EXPECT_THAT(Splitter.split("FOOBAR"), testing::ElementsAre("FOOBAR"));
-  EXPECT_THAT(Splitter.split("fooBAR"), testing::ElementsAre("foo", "BAR"));
-  EXPECT_THAT(Splitter.split("FOObar"), testing::ElementsAre("FOObar"));
+  EXPECT_THAT(Splitter.split("FOOBAR"),
+              testing::UnorderedElementsAre("FOOBAR"));
+  EXPECT_THAT(Splitter.split("fooBAR"),
+              testing::UnorderedElementsAre("foo", "BAR"));
+  EXPECT_THAT(Splitter.split("FOObar"),
+              testing::UnorderedElementsAre("FOObar"));
+
+  EXPECT_THAT(Splitter.split("foo_bar_bar"),
+              testing::UnorderedElementsAre("foo", "bar"));
 }
