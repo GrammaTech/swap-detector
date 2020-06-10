7ece66b7bdf61867f586d5d574ded4c2e091bf14
Aaron Ballman <aballman@grammatech.com>
2020-06-10 14:31:06 -0400

    Trying to fix CI again.
    IP:STAMP

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir build
 cd build
-cmake ../code/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=../../llvm-install/lib/cmake
+cmake ../code/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10/lib/cmake
 cmake --build .
 bin/TestSwappedArgsCpp
