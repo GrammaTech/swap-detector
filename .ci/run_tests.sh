c194be5948478dbb4d0af3d646dc4feadea01035
Aaron Ballman <aballman@grammatech.com>
2020-06-11 13:03:21 -0400

    Run the lit tests as well as the unit test.
    IP:STAMP

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -7,3 +7,4 @@ cd build
 cmake ../code/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10/lib/cmake
 cmake --build .
 bin/TestSwappedArgsCpp
+cmake --build . --target check-all
