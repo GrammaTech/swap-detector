a7847fc6580efe82ec511cf24bfe1f5b1cb8ff60
Aaron Ballman <aballman@grammatech.com>
2020-06-11 13:18:19 -0400

    Fix running the lit test.
    IP:STAMP

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir build
 cd build
-cmake ../code/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10/lib/cmake
+cmake ../code/ -DLLVM_EXTERNAL_LIT=$(which lit) -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10/lib/cmake
 cmake --build .
 bin/TestSwappedArgsCpp
 cmake --build . --target check-all
