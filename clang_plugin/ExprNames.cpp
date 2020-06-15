6249b9483564a6db777716340e7531dbb412e938
Aaron Ballman <aballman@grammatech.com>
2020-06-15 09:25:23 -0400

    Add contract and license info to all source files.
    Also renames some files to be a bit more consistent with our naming scheme.
    
    IP:STAMP

--- clang_plugin/ExprNames.cpp
+++ clang_plugin/ExprNames.cpp
@@ -1,8 +1,19 @@
+//===- ExprNamse.cpp --------------------------------------------*- C++ -*-===//
 //
-// Created by Joseph Ranieri on 2018-11-06.
+//  Copyright (C) 2020 GrammaTech, Inc.
 //
-
-#include "ExprNames.h"
+//  This code is licensed under the MIT license. See the LICENSE file in the
+//  project root for license terms.
+//
+// This material is based on research sponsored by the Department of Homeland
+// Security (DHS) Office of Procurement Operations, S&T acquisition Division via
+// contract number 70RSAT19C00000056. The views and conclusions contained herein
+// are those of the authors and should not be interpreted as necessarily
+// representing the official policies or endorsements, either expressed or
+// implied, of the Department of Homeland Security.
+//
+//===----------------------------------------------------------------------===//
+#include "ExprNames.hpp"
 #include <clang/Lex/Lexer.h>
 using namespace clang;
 
--- test/cpp/Checker.test.cpp
+++ test/cpp/Checker.test.cpp
@@ -1,3 +1,19 @@
+//===- Checker.test.cpp -----------------------------------------*- C++ -*-===//
+//
+//  Copyright (C) 2020 GrammaTech, Inc.
+//
+//  This code is licensed under the MIT license. See the LICENSE file in the
+//  project root for license terms.
+//
+// This material is based on research sponsored by the Department of Homeland
+// Security (DHS) Office of Procurement Operations, S&T acquisition Division via
+// contract number 70RSAT19C00000056. The views and conclusions contained herein
+// are those of the authors and should not be interpreted as necessarily
+// representing the official policies or endorsements, either expressed or
+// implied, of the Department of Homeland Security.
+//
+//===----------------------------------------------------------------------===//
+
 #include "SwappedArgChecker.hpp"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
